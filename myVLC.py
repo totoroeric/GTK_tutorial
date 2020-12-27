46fba260e0e3e463cb8e196b33615122e3469277
eric <totoroeric@gmail.com>
2020-12-27 21:54:23 +0800

    edit myVLC.py

--- myVLC.py
+++ myVLC.py
@@ -6,45 +6,14 @@ UI_INFO = """
 <ui>
   <menubar name='MenuBar'>
     <menu action='MediaMenu'>
-      <separator />
       <menuitem action='OpenFile' />
-      <menuitem action='OpenMultipleFile' />
-      <menuitem action='OpenDirectory' />
-      <menuitem action='OpenDisc' />
-      <menuitem action='OpenNetworkStream' />
-      <menuitem action='OpenCaptureDevice' />
-      <menuitem action='OpenLocationFromClipboard' />
-      <menu action='OpenRecentMedia'>
-        <menuitem action='FileNewStandard' />
-        <menuitem action='FileNewFoo' />
-        <menuitem action='FileNewGoo' />
-      </menu>
       <separator />
-      <menuitem action='SavePlaylistToFile' />
-      <menuitem action='ConvertOrSave' />
-      <menuitem action='Stream' />
-      <separator />
-      <menuitem action='QuitAtTheEndOfPlaylist' />
       <menuitem action='Quit' />
     </menu>
     <menu action='PlaybackMenu'>
-      <menu action='Title'>
-      </menu>
-      <menu action='Chapter'>
-      </menu>
-      <menu action='Program'>
-      </menu>
-      <menu action='CustomBookmarks'>
-      </menu>
-      <separator />
-      <menu action='Renderer'>
-        <menuitem action='Local' />
-        <separator />
-        <menuitem action='Scanning' />
-      </menu>
-      <separator />
-      <menu action='Speed'>
-      </menu>
+      <menutiem action='SpeedUp' />
+      <menuitem action='SpeedDown' />
+      <menuitem action='ResetSpeed' />
       <separator />
       <menuitem action='JumpForward' />
       <menuitem action='JumpBackward' />
@@ -52,37 +21,25 @@ UI_INFO = """
       <separator />
       <menuitem action='Play' />
       <menuitem action='Stop' />
-      <menuitem action='Previous' />
-      <menuitem action='Next' />
-      <menuitem action='Record' />
     </menu>
-    <menu action='Audio'>
-      <menu action='AudioTrack'>
-      </menu>
-      <menu action='AudioDevice'>
-      </menu>
-      <menu action='StereoMode'>
-      </menu>
+    <menu action='AudioMenu'>
+      <menuitem action='AudioTrackNext' />
       <separator />
-      <menu action='Visualizations'>
-      </menu>
-      <separator />
-      <menuitem action='IncreaseVolume'/>
-      <menuitem action='DecreaseVolume'/>
-      <menuitem action='Mute'/>
+      <menuitem action='IncreaseVolume' />
+      <menuitem action='DecreaseVolume' />
+      <menuitem action='Mute' />
     </menu>
-    <menu action='Video'>
+    <menu action='VideoMenu'>
     </menu>
-    <menu action='Subtitle'>
-      <menuitem action='AddSubtitleFile'/>
-      <menu action='SubTrack'>
-      </menu>
+    <menu action='SubtitleMenu'>
+      <menuitem action='AddSubtitleFile' />
+      <menuitem action='SubTrackNext' />
     </menu>
-    <menu action='Tools'>
+    <menu action='ToolsMenu'>
     </menu>
-    <menu action='View'>
+    <menu action='ViewMenu'>
     </menu>
-    <menu action='Help'>
+    <menu action='HelpMenu'>
     </menu>
   </menubar>
   <toolbar name='ToolBar'>
@@ -98,6 +55,11 @@ UI_INFO = """
     <toolitem action='MuteButton' />
     <toolitem action='VolumeController' />
   </toolbar>
+  <popup name='PopupMenu'>
+    <menuitem action='EditCopy' />
+    <menuitem action='EditPaste' />
+    <menuitem action='EditSomething' />
+  </popup>
 </ui>
 """
 
@@ -107,13 +69,126 @@ class myVLCWindow(Gtk.Window):
 
         self.set_default_size(600, 400)
 
-        action_group = Gtk.AccelGroup(name="my_actions")
+        action_group = Gtk.ActionGroup(name="my_actions")
 
         self.add_MediaMenu_actions(action_group)
-        self.add_MediaMenu_actions(action_group)
-        self.add_MediaMenu_actions(action_group)
-        self.add_MediaMenu_actions(action_group)
-        self.add_MediaMenu_actions(action_group)
-        self.add_MediaMenu_actions(action_group)
-        self.add_MediaMenu_actions(action_group)
-        self.add_MediaMenu_actions(action_group)
+        self.add_PlaybackMenu_actions(action_group)
+        self.add_Audio_actions(action_group)
+        self.add_Video_actions(action_group)
+        self.add_Subtitle_actions(action_group)
+        self.add_Tools_actions(action_group)
+        self.add_View_actions(action_group)
+        self.add_Help_actions(action_group)
+
+        uimanager = self.create_ui_manager()
+        uimanager.insert_action_group(action_group)
+
+        menubar = uimanager.get_widget("/MenuBar")
+        toolbar = uimanager.get_widget("/ToolBar")
+        eventbox = Gtk.EventBox()
+        eventbox.connect("button-press-event", self.on_button_press_event)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.pack_start(menubar, False, False, 0)
+        box.pack_start(eventbox, True, True, 0)  #这里需要插入视频窗口,这里用一个eventbox替代先
+        #这里插入进度条
+        box.pack_start(toolbar, False, False, 0)
+
+        label = Gtk.Label(label="Right-click to see the popup menu.")
+        eventbox.add(label)
+
+        self.popup = uimanager.get_widget("/PopupMenu")
+
+        self.add(box)
+    
+    def add_MediaMenu_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("MediaMenu", None, "Media", "<alt>M", None, None),
+                ("OpenFile", None, "Open File...", "<control>O", None, self.on_open_file),
+                ("Quit", None, "Quit", "<control>Q", None, self.on_quit)
+            ]
+        )
+
+    def add_PlaybackMenu_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("PlaybackMenu", None, "Playback", "<alt>L", None, None),
+                ('SpeedUp', None, "Speed Up", "<alt>U", None, None),
+                ('SpeedDown', None, "Speed Down", "<alt>D", None, None),
+                ('ResetSpeed', None, "Reset Speed", "<alt>R", None, None),
+                ('JumpForward', None, "Jump Forward", "<alt>J", None, None),
+                ('JumpBackward', None, "Jump Backward", "<alt>K", None, None),
+                ('JumpToSpecificTime', None, "Jump To Specific Time", "<control>T", None, None),
+                ('Play', None, "Play", "<alt>P", None, None),
+                ('Stop', None, "Stop", "<alt>S", None, None)
+            ]
+        )
+
+    def add_Audio_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("AudioNenu", None, "Audio", "<alt>A", None, None),
+                ("AudioTrackNext", None, "Audio Track", "<alt>T", None, None),
+                ("IncreaseVolume", None, "Increase Volume", "<alt>I", None, None),
+                ("DecreaseVolume", None, "Decrease Volume", "<alt>E", None, None),
+                ("Mute", None, "Mute", "<alt>M", None, None)
+            ]
+        )
+
+    def add_Video_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("VideoNenu", None, "Video", "<alt>V", None, None)
+            ]
+        )
+
+
+    def add_Subtitle_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("SubtitleMenu", None, "Subtitle", "<alt>T", None, None),
+                ("AddSubtitleFile", None, "Add Subtitle File...", "<alt>S", None, None),
+                ("SubTrackNext", None, "Sub Track Next", "<alt>R", None, None)
+            ]
+        )
+    def add_Tools_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("ToolsMenu", None, "Tools", None, None, None)
+            ]
+        )
+    def add_View_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("ViewMenu", None, "Video", None, None, None)
+            ]
+        )
+    def add_Help_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("HelpNenu", None, "Help", None, None, None)
+            ]
+        )
+
+    def create_ui_manager(self):
+        uimanager = Gtk.UIManager()
+
+        # Throws exception if something went wrong
+        uimanager.add_ui_from_string(UI_INFO)
+
+        # Add the accelerator group to the toplevel window
+        accelgroup = uimanager.get_accel_group()
+        self.add_accel_group(accelgroup)
+        return uimanager
+
+    def on_open_file(self, widget):
+        pass
+
+    def on_quit(self, widget):
+        Gtk.main_quit()
+
+window = myVLCWindow()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+Gtk.main()
