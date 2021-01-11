24cc06e77a70fbe29b64821476b4851759fc0d25
eric <totoroeric@gmail.com>
2021-01-11 16:55:37 +0800

    trying treeviews

--- srt-demo.py
+++ srt-demo.py
@@ -1,11 +1,14 @@
 from srt import parse
 
-with open('/home/eric/Downloads/S01E16.eng.srt') as f:
+with open('/home/eric/Downloads/S01E22.eng.srt') as f:
     read_data = f.read()
     # print(read_data)
     subs = parse(read_data)
     # print(list(subs))
     for sub in subs:
-        print(sub)
+        print("index: ", sub.index)
+        print("start: ", sub.start)
+        print("end: ", sub.end)
+        print("content: ", sub.content)
         print("\n")
 
